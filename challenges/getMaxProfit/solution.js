674298dbdf009840eb6ecaa3fe0d9cc6da414e96
jtamsut <jtamsut1993@gmail.com>
2018-03-19 11:49:15 -0700

    update getMaxProfit

--- challenges/getMaxProfit/solution.js
+++ challenges/getMaxProfit/solution.js
@@ -15,7 +15,5 @@ function getMaxProfit(prices) {
     return maxProfit;
 }
 
-console.log(getMaxProfit([5,3,2,1,0])) //=>  -1
-console.log(getMaxProfit([10, 7, 5, 8, 11, 9])) //=> 6
-console.log(getMaxProfit([-1, 3, 4, 0, 11, 9])) //=> 6
+
 
