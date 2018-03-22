9b3a39949c8f8b7cd83af6140cf8db017474b005
jtamsut <jtamsut1993@gmail.com>
2018-03-22 11:42:04 -0700

    add solution to rectangle intersection

--- challenges/rectangleIntersection/rectangleIntersection.md
+++ challenges/rectangleIntersection/rectangleIntersection.md
@@ -1,8 +1,26 @@
 ## rectangleIntersection
 
-**Difficulty**: Advanced
+**Difficulty**: Medium
 
 ## Prompt 
+
+Write a function called `rectangleIntersection` that accepts two objects as input. Each object contains information about a rectangle. More specifically each object looks like the one below: 
+
+```js
+let rectangle = {
+
+    // coordinates of bottom-left corner
+    leftX: 1,
+    bottomY: 1,
+
+    // width and height
+    width: 6,
+    height: 3,
+
+};
+```
+
+The values above can be thought of as points on a Cartesian coordinate plane. `rectangleIntersection` should return the area in which the two rectangles overlap.
 
 
 ## Examples
--- challenges/rectangleIntersection/solution.js
+++ challenges/rectangleIntersection/solution.js
@@ -1,45 +1,18 @@
 function rectangleIntersection(rect1, rect2) {
+    
     let leftX = Math.max(rect1.leftX, rect2.leftX);
     let rightX = Math.min(rect1.leftX + rect1.width, rect2.leftX + rect2.width);
  
-    console.log(leftX, rightX);
-    rect1.rightX = rect1.leftX + rect1.width;
-    rect2.rightX = rect2.leftX + rect2.width;
+    let bottomY = Math.max(rect1.bottomY, rect2.bottomY);
+    let topY = Math.min(rect1.bottomY + rect1.height, rect2.bottomY + rect2.height);
 
-    rect1.topY = rect1.bottomY + rect1.height;
-    rect2.topY = rect2.bottomY + rect2.height;
-    
-    // A intersects B
-    if ( (rect2.leftX > rect1.leftX) === (rect1.rightX < rect2.rightX) ) {
-        if ( (rect2.bottomY > rect1.bottomY) === (rect1.topY < rect2.topY) ) {
-            // return (rect1.rightX - rect2.leftX) * (rect1.topY - rect2.bottomY);
-            // return (rect2.rightX - rect1.leftX) * (rect2.topY - rect1.bottomY);
-        }
-    }
+    let width = rightX - leftX;
+    let height = topY - bottomY; 
 
-    // B completely in A
-    if ( !(rect2.leftX > rect1.leftX) && !(rect1.rightX < rect2.rightX) ) {
-        if ( !(rect2.bottomY > rect1.bottomY) && !(rect1.topY < rect2.topY) ) {
-            return (rect2.rightX - rect1.leftX) * (rect2.topY - rect1.bottomY);
-        }
-    }
-
+    if (width > 0 && height > 0) {
+        return width * height;
+    } 
     
     return false;
+
 }
-
-let rectA = { 
-    leftX: 1, 
-    bottomY: 1, 
-    width: 10, 
-    height: 10
-};
-
-let rectB = { 
-    leftX: 2, 
-    bottomY: 2, 
-    width: 2, 
-    height: 2
-};
-
-console.log(rectangleIntersection(rectB, rectA));
