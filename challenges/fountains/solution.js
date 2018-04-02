7041436c0b07a7d0add32134d8c2923f4249513a
nicktayl0r <nicholasjt22@gmail.com>
2018-04-02 12:41:00 -0700

    Update solution.js

--- challenges/fountains/solution.js
+++ challenges/fountains/solution.js
@@ -1,11 +1,1 @@
-function Fountain(heights, fountains){
-    if (heights.length !== fountains.length) {return 'bollocks'}
-    let f = fountains.indexOf(1), flooded = [];
-    flooded[f] = 1;
-    for(let i = 0; i < heights.length; i++){
-      if (heights[f+i] <= heights[f] && flooded[f+i-1]) {flooded[f+i] = 1}
-      if (heights[f-i] <= heights[f] && flooded[f-i+1]) {flooded[f-i] = 1}
-      if (flooded[i] === undefined){ flooded[i] = 0}
-    }
-    return flooded;
-  }
+
